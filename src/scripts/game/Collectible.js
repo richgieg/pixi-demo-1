89916717307be2abe71af3c13395af9f3128e0c3
Richard Gieg <richgieg@users.noreply.github.com>
2024-11-04 10:44:51 -0800

    Add floating animation to collectibles

--- src/scripts/game/Collectible.js
+++ src/scripts/game/Collectible.js
@@ -1,23 +1,38 @@
 import * as Matter from 'matter-js';
+import { gsap } from "gsap";
 import { App } from '../system/App';
 
 export class Collectible {
     constructor(kind, value, platformX, platformTileSize, y) {
-        this.createSprite(kind, platformX, platformTileSize, y);
-        App.app.ticker.add(this.update, this);
         this.value = value;
+        this.y = y;
+        this.createSprite(kind, platformX, platformTileSize);
+        App.app.ticker.add(this.update, this);
+        this.startFloating();
     }
 
-    createSprite(kind, platformX, platformTileSize, y) {
+    createSprite(kind, platformX, platformTileSize) {
         this.sprite = App.sprite(kind);
         // Center the collectible within the platform tile.
         this.sprite.x = platformX + ((platformTileSize - this.sprite.width) / 2);
-        this.sprite.y = y;
+        this.sprite.y = this.y;
+    }
+
+    startFloating() {
+        gsap.to(this, {
+            y: this.y - 2,
+            duration: 0.25,
+            ease: "sine.inOut",
+            repeat: -1,
+            yoyo: true,
+            delay: Math.random() * 0.25,
+        });
     }
 
     update() {
         if (this.sprite) {
-            Matter.Body.setPosition(this.body, {x: this.sprite.width / 2 + this.sprite.x + this.sprite.parent.x, y: this.sprite.height / 2 + this.sprite.y + this.sprite.parent.y});
+            this.sprite.y = this.y;
+            Matter.Body.setPosition(this.body, {x: this.sprite.width / 2 + this.sprite.x + this.sprite.parent.x, y: this.sprite.height / 2 + this.y + this.sprite.parent.y});
         }
     }
 
@@ -34,6 +49,7 @@ export class Collectible {
             Matter.World.remove(App.physics.world, this.body);
             this.sprite.destroy();
             this.sprite = null;
+            gsap.killTweensOf(this);
         }
     }
 }
