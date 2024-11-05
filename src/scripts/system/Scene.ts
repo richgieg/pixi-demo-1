d11b48452b47e85092ab8936bba6f002594a99de
Richard Gieg <richgieg@users.noreply.github.com>
2024-11-04 20:23:14 -0800

    Convert Scene.js to Scene.ts

---
+++ src/scripts/system/Scene.ts
@@ -0,0 +1,19 @@
+import * as PIXI from "pixi.js";
+import { App } from "./App";
+
+export abstract class Scene {
+    container: PIXI.Container;
+
+    constructor() {
+        this.container = new PIXI.Container();
+        this.container.interactive = true;
+        this.create();
+        App.app.ticker.add(this.update, this);
+    }
+
+    abstract create(): void;
+
+    abstract update(dt: number): void;
+    
+    abstract destroy(): void;
+}
