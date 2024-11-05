e5afe7aef47f21b6968b0dec27abdb081a163c46
Richard Gieg <richgieg@users.noreply.github.com>
2024-11-04 20:13:49 -0800

    Convert Loader.js to Loader.ts

---
+++ src/scripts/system/Loader.ts
@@ -0,0 +1,30 @@
+import { Config } from "scripts/game/Config";
+
+export class Loader {
+    loader: any;
+    config: Config;
+    resources: any;
+
+    constructor(loader: any, config: Config) {
+        this.loader = loader;
+        this.config = config;
+        this.resources = {};
+    }
+
+    preload() {
+        for (const asset of this.config.loader) {
+            let key = asset.key.substr(asset.key.lastIndexOf('/') + 1);
+            key = key.substring(0, key.indexOf('.'));
+            if (asset.key.indexOf(".png") !== -1 || asset.key.indexOf(".jpg") !== -1) {
+                this.loader.add(key, asset.data.default)
+            }
+        }
+
+        return new Promise<void>(resolve => {
+            this.loader.load((loader: any, resources: any) => {
+                this.resources = resources;
+                resolve();
+            });
+        });
+    }
+}
